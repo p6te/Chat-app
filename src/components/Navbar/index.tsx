bf340e675904fe1862d57fa95fe0f9b1da241eb1
Piotr Matlak <piotr.matlak.contact@gmail.com>
2023-11-02 20:59:14 +0100

    fix user name

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,23 +1,27 @@
 import { useNavigate } from "react-router-dom";
 import FirebaseAuthService from "../../firebaseAuthService";
 import "./styles.scss";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const { currentUser } = useContext(AuthContext);
+
   const handleLogout = () => {
     FirebaseAuthService.logoutUser();
     navigate("/login");
   };
+  if (!currentUser?.photoURL) {
+    return;
+  }
   return (
     <div className="navbar">
       <h4>Wall chat</h4>
 
       <div className="user">
-        <img
-          src="https://cdn.pixabay.com/photo/2018/02/21/08/40/woman-3169726_640.jpg"
-          alt=""
-        />
-        <span>Name</span>
+        <img src={currentUser?.photoURL} alt="" />
+        <span>{currentUser?.displayName}</span>
         <button onClick={handleLogout}>logout</button>
       </div>
     </div>
